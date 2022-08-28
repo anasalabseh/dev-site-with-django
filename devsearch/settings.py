8ab9ac9603e9a42062b20c9c7040486353204839
Anas al-Abseh <anas.abseh@gmail.com>
2022-08-29 00:21:52 +0300

    building RESTful APIs -before-

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'project.apps.ProjectConfig',
     'users.apps.UsersConfig',
+    
+    'rest_framework'
 
 
 ]
--- devsearch/urls.py
+++ devsearch/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/',include('project.url')),
-    path('', include('users.url'))
+    path('', include('users.url')),
+    path('api/', include('api.urls')),
+    
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
